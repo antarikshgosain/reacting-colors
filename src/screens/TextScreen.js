52f7ff1258ab50011503ab08a8b406f7f3c89b83
antarikshgosain <antariksh.gosain@gmail.com>
2025-05-05 22:27:35 -0400

    text input with conditions

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import { Text, StyleSheet, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
 const TextScreen = () => { 
+    const [name, setName] = useState('');
+    console.log(`name is ${name}`);
     return (
         <View>
+            <Text style={{ fontSize: 30, marginHorizontal: 10 }}>Enter Name:</Text>
             <TextInput 
                 style={styles.input} 
                 autoCapitalize="none"
                 autoCorrect={false}
+                value={name}
+                onChangeText={newValue => setName(newValue)}
             />
+            
+            { name.length < 5 ? 
+                <Text>
+                    Name should be at least 5 charaters
+                </Text> : null
+            }
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     input: {
-        margin: 15,
+        margin: 10,
         borderColor: "black",
         borderWidth: 2,
         fontSize: 15,
